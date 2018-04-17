4e86da0a5c019317bcb008007f1ce25b2fb6693e
Paul J. Durack <durack1@llnl.gov>
2018-04-17 10:28:39 -0700

    Update upstream; Revise source_id PCMDI-AMIP-1-1-4

--- src/writeJson.py
+++ src/writeJson.py
@@ -58,6 +58,7 @@ PJD  4 Apr 2018     - Update print statements for python3
 PJD  9 Apr 2018     - Added 'msftmz' and 'msftyz' to exclusion list for Omon
 PJD 10 Apr 2018     - Update modeling_realm for LIfx areacellg https://github.com/PCMDI/input4MIPs-cmor-tables/issues/39
 PJD 10 Apr 2018     - Register source_id PCMDI-AMIP-1-1-4 https://github.com/PCMDI/input4MIPs-cmor-tables/issues/47
+PJD 17 Apr 2018     - Revise source_id PCMDI-AMIP-1-1-4 https://github.com/PCMDI/input4MIPs-cmor-tables/issues/47
                     - TODO: Deal with lab cert issue https://raw.githubusercontent.com -> http://rawgit.com (see requests library)
 
 @author: durack1
@@ -542,13 +543,13 @@ source_id[key]['realm'] = 'ocean'
 source_id[key]['references'] = 'Taylor, K.E., D. Williamson and F. Zwiers, 2000: The sea surface temperature and sea ice concentration boundary conditions for AMIP II simulations. PCMDI Report 60, Program for Climate Model Diagnosis and Intercomparison, Lawrence Livermore National Laboratory, 25 pp. Available online: https://pcmdi.llnl.gov/report/pdf/60.pdf'
 source_id[key]['region'] = ['global_ocean']
 source_id[key]['release_year'] = '2018'
-source_id[key]['source_description'] = 'PCMDI (pcmdi-cmip@llnl.gov)'
+source_id[key]['source_description'] = 'Sea surface temperature and sea-ice datasets produced by PCMDI (LLNL) for the AMIP (DECK) experiment of CMIP6'
 source_id[key]['source'] = 'PCMDI-AMIP 1.1.4: Merged SST based on UK MetOffice HadISST and NCEP OI2'
 source_id[key]['source_id'] = key
 source_id[key]['source_type'] = 'satellite_blended'
 source_id[key]['source_variables'] = ['areacello','sftof','siconc','siconcbcs',
                                       'tos','tosbcs']
-source_id[key]['source_version_number'] = '1.1.4'
+source_id[key]['source_version'] = '1.1.4'
 source_id[key]['target_mip'] = 'CMIP'
 source_id[key]['title'] = 'PCMDI-AMIP 1.1.4 dataset prepared for input4MIPs'
 #----
